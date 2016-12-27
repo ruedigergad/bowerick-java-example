a262d717d05f024b13937d242ac061152a9279c9
Ruediger Gad <r.c.g@gmx.de>
2016-12-27 20:22:05 +0100

    Initial commit.

---
+++ src/test/java/rgad/bowerick_java_example/test/SimpleMultiTransportTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2016, Ruediger Gad
+ * 
+ * This software is released under the terms of the Eclipse Public License
+ * (EPL) 1.0. You can find a copy of the EPL at:
+ * http://opensource.org/licenses/eclipse-1.0.php
+ * 
+ */
+package rgad.bowerick_java_example.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import bowerick.JmsConsumerCallback;
+import bowerick.JmsController;
+import bowerick.JmsProducer;
+import clj_assorted_utils.JavaUtils;
+
+/**
+ * 
+ * Simple example for illustrating the usage of bowerick with multiple
+ * transports from within Java.
+ * 
+ * @author Ruediger Gad &lt;r.c.g@gmx.de&gt;
+ *
+ */
+public class SimpleMultiTransportTest {
+
+	private static final List<String> TRANSPORT_URLS = Collections.unmodifiableList(Arrays.asList(new String[] {
+			"stomp://127.0.0.1:1701", "tcp://127.0.0.1:1864", "ws://127.0.0.1:8472", "mqtt://127.0.0.1:2000" }));
+	private static final String TEST_TOPIC = "/topic/test.topic.foo";
+
+	private JmsController jmsController;
+
+	@Before
+	public void setUp() {
+		jmsController = new JmsController(TRANSPORT_URLS);
+		jmsController.startEmbeddedBroker();
+	}
+
+	@After
+	public void tearDown() {
+		jmsController.stopEmbeddedBroker();
+	}
+
+	@Test
+	public void sendAndReceiveStringViaJson() throws InterruptedException {
+		final CountDownLatch cdl = new CountDownLatch(TRANSPORT_URLS.size());
+
+		final List<StringBuffer> received = new ArrayList<>();
+		for (int i = 0; i < TRANSPORT_URLS.size(); i++) {
+			received.add(new StringBuffer());
+			final int idx = i;
+
+			JmsController.createJsonConsumer(TRANSPORT_URLS.get(idx), TEST_TOPIC, new JmsConsumerCallback() {
+
+				public void processData(Object data) {
+					received.get(idx).append((String) data);
+					cdl.countDown();
+				}
+			}, 1);
+		}
+
+		JmsProducer producer = JmsController.createJsonProducer("stomp://127.0.0.1:1701", TEST_TOPIC, 1);
+		producer.sendData("Test String");
+
+		cdl.await();
+
+		for (int i = 0; i < received.size(); i++) {
+			assertEquals("Test String", received.get(i).toString());
+		}
+	}
+
+	@Test
+	public void sendAndReceiveListViaJson() throws InterruptedException {
+		final CountDownLatch cdl = new CountDownLatch(TRANSPORT_URLS.size());
+
+		final List<List<Object>> received = new ArrayList<>();
+		for (int i = 0; i < TRANSPORT_URLS.size(); i++) {
+			received.add(new ArrayList<Object>());
+			final int idx = i;
+
+			JmsController.createJsonConsumer(TRANSPORT_URLS.get(idx), TEST_TOPIC, new JmsConsumerCallback() {
+
+				public void processData(Object data) {
+					received.get(idx).addAll((List<?>) JavaUtils.convertFromClojureToJava(data));
+					cdl.countDown();
+				}
+			}, 1);
+		}
+
+		JmsProducer producer = JmsController.createJsonProducer("stomp://127.0.0.1:1701", TEST_TOPIC, 1);
+
+		List<Object> testData = new ArrayList<>();
+		testData.add("Test String");
+		testData.add(42);
+		testData.add(1.23456789);
+		testData.add(true);
+
+		producer.sendData(testData);
+
+		cdl.await();
+
+		for (int i = 0; i < received.size(); i++) {
+			assertNotSame(testData, received.get(i));
+			assertEquals(testData, received.get(i));
+		}
+	}
+
+	@Test
+	public void sendAndReceiveMapViaJson() throws InterruptedException {
+		final CountDownLatch cdl = new CountDownLatch(TRANSPORT_URLS.size());
+		final List<Map<String, Object>> received = new ArrayList<>();
+
+		for (int i = 0; i < TRANSPORT_URLS.size(); i++) {
+			received.add(new HashMap<String, Object>());
+			final int idx = i;
+
+			JmsController.createJsonConsumer(TRANSPORT_URLS.get(idx), TEST_TOPIC, new JmsConsumerCallback() {
+
+				public void processData(Object data) {
+					received.get(idx).putAll((Map<String, ?>) JavaUtils.convertFromClojureToJava(data));
+					cdl.countDown();
+				}
+			}, 1);
+		}
+
+		JmsProducer producer = JmsController.createJsonProducer("stomp://127.0.0.1:1701", TEST_TOPIC, 1);
+
+		Map<String, Object> testData = new HashMap<>();
+		testData.put("SomeString", "Test String");
+		testData.put("SomeInt", 42);
+		testData.put("SomeFloat", 1.23456789);
+		testData.put("SomeBoolean", true);
+
+		producer.sendData(testData);
+
+		cdl.await();
+
+		for (int i = 0; i < received.size(); i++) {
+			assertNotSame(testData, received.get(i));
+			assertEquals(testData, received.get(i));
+		}
+	}
+}
---
+++ src/test/java/rgad/bowerick_java_example/test/SimpleSingleTransportWebSocketStompTest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2016, Ruediger Gad
+ * 
+ * This software is released under the terms of the Eclipse Public License
+ * (EPL) 1.0. You can find a copy of the EPL at:
+ * http://opensource.org/licenses/eclipse-1.0.php
+ * 
+ */
+package rgad.bowerick_java_example.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import bowerick.JmsConsumerCallback;
+import bowerick.JmsController;
+import bowerick.JmsProducer;
+
+/**
+ * 
+ * Simple example for illustrating the usage of bowerick, using the example of
+ * Stomp via WebSockets, from within Java.
+ * 
+ * @author Ruediger Gad &lt;r.c.g@gmx.de&gt;
+ *
+ */
+public class SimpleSingleTransportWebSocketStompTest {
+
+	private static final String STOMP_TRANSPORT_URL = "ws://127.0.0.1:1701";
+	private static final String TEST_TOPIC = "/topic/test.topic.foo";
+
+	private JmsController jmsController;
+
+	@Before
+	public void setUp() {
+		jmsController = new JmsController(STOMP_TRANSPORT_URL);
+		jmsController.startEmbeddedBroker();
+	}
+
+	@After
+	public void tearDown() {
+		jmsController.stopEmbeddedBroker();
+	}
+
+	@Test
+	public void sendAndReceiveStringViaJson() throws InterruptedException {
+		final CountDownLatch cdl = new CountDownLatch(1);
+		final StringBuffer received = new StringBuffer();
+
+		JmsController.createJsonConsumer(STOMP_TRANSPORT_URL, TEST_TOPIC, new JmsConsumerCallback() {
+
+			public void processData(Object data) {
+				received.append((String) data);
+				cdl.countDown();
+			}
+		}, 1);
+
+		JmsProducer producer = JmsController.createJsonProducer(STOMP_TRANSPORT_URL, TEST_TOPIC, 1);
+		producer.sendData("Test String");
+
+		cdl.await();
+
+		assertEquals("Test String", received.toString());
+	}
+
+	@Test
+	public void sendAndReceiveListViaJson() throws InterruptedException {
+		final CountDownLatch cdl = new CountDownLatch(1);
+		final List<Object> received = new ArrayList<>();
+
+		JmsController.createJsonConsumer(STOMP_TRANSPORT_URL, TEST_TOPIC, new JmsConsumerCallback() {
+
+			public void processData(Object data) {
+				received.addAll((List<?>) data);
+				cdl.countDown();
+			}
+		}, 1);
+
+		JmsProducer producer = JmsController.createJsonProducer(STOMP_TRANSPORT_URL, TEST_TOPIC, 1);
+
+		List<Object> testData = new ArrayList<>();
+		testData.add("Test String");
+		testData.add(42);
+		testData.add(1.23456789);
+		testData.add(true);
+
+		producer.sendData(testData);
+
+		cdl.await();
+
+		assertNotSame(testData, received);
+		assertEquals(testData, received);
+	}
+
+	@Test
+	public void sendAndReceiveMapViaJson() throws InterruptedException {
+		final CountDownLatch cdl = new CountDownLatch(1);
+		final Map<String, Object> received = new HashMap<>();
+
+		JmsController.createJsonConsumer(STOMP_TRANSPORT_URL, TEST_TOPIC, new JmsConsumerCallback() {
+
+			public void processData(Object data) {
+				received.putAll((Map<String, ?>) data);
+				cdl.countDown();
+			}
+		}, 1);
+
+		JmsProducer producer = JmsController.createJsonProducer(STOMP_TRANSPORT_URL, TEST_TOPIC, 1);
+
+		Map<String, Object> testData = new HashMap<>();
+		testData.put("SomeString", "Test String");
+		testData.put("SomeInt", 42);
+		testData.put("SomeFloat", 1.23456789);
+		testData.put("SomeBoolean", true);
+
+		producer.sendData(testData);
+
+		cdl.await();
+
+		assertNotSame(testData, received);
+		assertEquals(testData, received);
+	}
+}
